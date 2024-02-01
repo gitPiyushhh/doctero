035acf24a61cadc7e0efde9c6bed58ee5d37d119
Piyush <Piyushhhagarwal@gmail.com>
2024-02-01 05:51:21 +0530

    Web socket server created

--- socket.js
+++ socket.js
@@ -7,38 +7,58 @@ const io = new Server(9000, {
 const userToSocketId = new Map();
 const socketIdToUser = new Map();
 
+/*
+  Event handlers
+*/
+function handleRoomJoin(io, socket, userToSocketId, socketIdToUser, data) {
+  const { userId: user, room } = data;
+  userToSocketId.set(user, socket.id);
+  socketIdToUser.set(socket.id, user);
+
+  io.to(room).emit('user:joined', { user, id: socket.id });
+
+  socket.join(room);
+  io.to(socket.id).emit('room:join', data);
+}
+
+function handleUserCall(io, socket, to, offer) {
+  console.log('Remote offer:', offer);
+  io.to(to).emit('call:incoming', { from: socket.id, offer });
+}
+
+function handleCallAccepted(io, socket, to, answer) {
+  io.to(to).emit('call:accepted', { from: socket.id, answer });
+}
+
+function handleNegoNeeded(io, socket, to, offer) {
+  io.to(to).emit('peer:nego:needed', { from: socket.id, offer });
+}
+
+function handleNegoDone(io, socket, to, ans) {
+  io.to(to).emit('peer:nego:final', { from: socket.id, ans });
+}
+
+/*
+  Socket signals
+*/
 io.on('connection', (socket) => {
-  /*
-    Socket events
-  */
-
   socket.on('room:join', (data) => {
-    const { userId: user, room } = data;
-    userToSocketId.set(user, socket.id);
-    socketIdToUser.set(socket.id, user);
-
-    // Emit event to give incoming user details
-    io.to(room).emit('user:joined', { user, id: socket.id });
-
-    // Pushing user to room
-    socket.join(room);
-    io.to(socket.id).emit('room:join', data);
+    handleRoomJoin(io, socket, userToSocketId, socketIdToUser, data);
   });
 
-  socket.on('user:call', function ({to, offer}) {
-    console.log("Remote offer:", offer);
-    io.to(to).emit("call:incoming", {from: socket.id, offer})
+  socket.on('user:call', ({ to, offer }) => {
+    handleUserCall(io, socket, to, offer);
   });
 
-  socket.on("call:accepted", ({to, answer}) => {
-    io.to(to).emit("call:accepted", {from: socket.id, answer})
-  })
+  socket.on('call:accepted', ({ to, answer }) => {
+    handleCallAccepted(io, socket, to, answer);
+  });
 
-  socket.on('peer:nego:needed', ({offer, to}) => {
-    io.to(to).emit("peer:nego:needed", {from: socket.id, offer})
-  })
+  socket.on('peer:nego:needed', ({ offer, to }) => {
+    handleNegoNeeded(io, socket, to, offer);
+  });
 
-  socket.on('peer:nego:done', ({to, ans}) => {
-    io.to(to).emit("peer:nego:final", {from: socket.id, ans})
-  })
+  socket.on('peer:nego:done', ({ to, ans }) => {
+    handleNegoDone(io, socket, to, ans);
+  });
 });
